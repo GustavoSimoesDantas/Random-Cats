8a8d381dbfdcbf8596b3028f0c3b0b5d2a313e34
Gustavo Dantas <gustavosimoesd@gmail.com>
2026-03-31 14:33:22 -0300

    feat: pagina de gatos aleatorios

---
+++ script.js
@@ -0,0 +1,16 @@
+async function loadCat() {
+    try {
+      const response = await fetch("https://api.thecatapi.com/v1/images/search");
+      const data = await response.json();
+  
+      const imageUrl = data[0].url;
+  
+      document.getElementById("catImage").src = imageUrl;
+  
+    } catch (error) {
+      console.log("Erro:", error);
+    }
+  }
+  
+  // carregar ao abrir a página
+  loadCat();
